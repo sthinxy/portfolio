8f49aa4037d16b8d51ba5cb41db30be818cae063
Beatriz <ws7499816@gmail.com>
2026-06-27 05:30:23 -0300

    Delete js directory

--- js/supabase-config.js
+++
@@ -1,7 +0,0 @@
-window.STHINXY_SUPABASE = {
-  enabled: true,
-  url: "https://nogppgtpgnuugqylduzk.supabase.co",
-  anonKey: "sb_publishable_a5IwEn0hVpEVjLcq--FqbA_swpQiWsw",
-  table: "site_settings",
-  rowId: "main"
-};
